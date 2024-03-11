8700b61df2b7c5df4bc13c0ed1ac0c120dc567a9
JoaoPauloFontana <jpfontana12@icloud.com>
2024-03-11 09:20:10 -0300

    refactor: tiping and add success flag

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,40 +7,81 @@ use App\Http\Requests\StoreTaskRequets;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
+        try {
+            $tasks = Task::all();
+        } catch (Throwable $th) {
+            Log::info($th);
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+
         return response()->json([
-            'tasks' => TaskResource::collection(Task::all()),
+            'success' => true,
+            'tasks' => TaskResource::collection($tasks),
         ]);
     }
 
-    public function store(StoreTaskRequest $request)
+    public function store(StoreTaskRequest $request): JsonResponse
     {
-        Task::create($request->validated());
+        try {
+            Task::create($request->validated());
+        } catch (Throwable $th) {
+            Log::info($th);
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
 
         return response()->json([
+            'success' => true,
             'message' => 'Task created successfully'
         ], 201);
     }
 
-    public function update(UpdateTaskRequest $request, Task $task)
+    public function update(UpdateTaskRequest $request, Task $task): JsonResponse
     {
-        $task->update($request->validated());
+        try {
+            $task->update($request->validated());
+        } catch (Throwable $th) {
+            Log::info($th);
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
 
         return response()->json([
+            'success' => true,
             'message' => 'Task updated successfully',
         ]);
     }
 
-    public function destroy(Task $task)
+    public function destroy(Task $task): JsonResponse
     {
-        $task->delete();
+        try {
+            $task->delete();
+        } catch (Throwable $th) {
+            Log::info($th);
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
 
         return response()->json([
+            'success' => true,
             'message' => 'Task deleted successfully',
         ]);
     }
